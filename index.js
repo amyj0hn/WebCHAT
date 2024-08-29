da059a581055aad37d4a4bc8b30ab3c98d875033
amyj0hn <amyj62450@gmail.com>
2024-08-29 14:56:40 +0200

    Controllers set up done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Imports
 import cors from  'cors';
-
+import { userRouter, express } from "./controller/userController.js"
+import {productRouter} from "./controller/productController.js"
+import { errorHandling } from './middleware/ErrorHandling.js';
+import path from "path";
 
 // Create an express app
 const app = express();
-const port = +process.env.PORT || 4001;
+const port = +process.env.PORT || 4000;
 
 // Middleware
 app.use((req, res, next) => {
@@ -27,7 +30,25 @@ app.use(
 )
 
 // Routes
+app.use('/User', userRouter)
+app.use('/Product', productRouter)
 
 // Endpoint
+app.get("^/$|/WebCHAT", (req, res)=>{
+    res.status(200).sendFile(path.resolve("./static/html/index.html"))
+})
+
+// any endpoint that we did not create will return this.
+app.get('*', (req, res) => {        
+    res.json({
+      status: 404,
+      msg: 'Resource not found'
+    })
+  })
+app.use(errorHandling)
 
 // Start the server
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
+
